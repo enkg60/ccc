25d7700c9e58c6b3ad08ec912717f868b71f1e6f
enkg60 <70356153+enkg60@users.noreply.github.com>
2024-02-08 19:56:39 -0500

    Update 2016j2.cpp

--- 2016/2016j2.cpp
+++ 2016/2016j2.cpp
@@ -3,6 +3,8 @@ using namespace std;
 //By: Emilio, 2024-02-08
 //Finished 7:51
 //Started around 7:15
+//Notes:
+//Could have less lines but needed to finish before i have to leave this hour
 int main() {
   int square[4][4];
   int sum = 0;
