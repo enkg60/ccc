4592ec056a66a96ad59dca3759c77fbc760ddc38
enkg60 <emiliomateoguevara@gmail.com>
2024-02-19 13:45:01 -0500

    worked on 2015j4

--- 2015/2015j4.cpp
+++ 2015/2015j4.cpp
@@ -7,30 +7,45 @@ int main(){
   int in;
   cin >> in;
   vector<vector<int>> tracking {
-    
-
   };
+
   stack<int> onHold;
   int passed = 0;
   for(int i = 0; i < in; i++){
-    string input;
-    cin >> input;
-    if(input[0] == "R"){
-      //new row
-      tracking.push_back({input[2], 0})
-      onHold.push(tracking.back());
-    }
-    if(input[0] == "W"){
-      while(!onHold.empty()){
-        tracking[onHold.top()][1]++
+    string line;
+    cin >> line;
+    char mode = line[0];
+    int other = int(line[2]);
+
+    if(mode == 'R'){
+      bool exsists = false;
+      int x;
+
+      //check if already exsists else;
+      
+      for(x = 0; x < tracking.size(); x++){
+        if(tracking[x][0] == other){
+          exsists = true;
+          break;
+        }
+      }
+      if(exsists){
+        break;
+      } else {
+        tracking.push_back(vector<int> p = {other, passed});
+        onHold.push(tracking.size() - 1);
       }
     }
-  }
-  for (int i = 0; i < tracking.size(); i++)  
-    { 
-        for (int j = 0; j < tracking[i].size(); j++) 
-        { 
-            cout << tracking[i][j] << " "; 
-        }     
+
+    if(mode == 'W'){
+      while(!onHold.empty()){
+        tracking[onHold.top()][1] += 1 + passed;
+        onHold.pop();
+      }
+      passed = 0;
     }
+
+
+    passed++;
+  }
 }
