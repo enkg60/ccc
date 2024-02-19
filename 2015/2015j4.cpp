3c9ebbb7b568c077bf4bdc1c48ede82b545a49cf
enkg60 <emiliomateoguevara@gmail.com>
2024-02-19 15:34:43 -0500

    Finished 2015j4.cpp. Explanation for code in 2015/2015j4.org

--- 2015/2015j4.cpp
+++ 2015/2015j4.cpp
@@ -1,51 +1,134 @@
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <string>
 using namespace std;
 
-int main(){
-  int in;
-  cin >> in;
-  vector<vector<int>> tracking {
-  };
+void vecout(vector<vector<int>> vec){
+  for(int x = 0; x < vec.size(); x++){
+    for(int y = 0; y < vec[x].size(); y++){
+      cout << vec[x][y] << " ";
+    }
+    cout << '\n';
+  }
+}
 
-  stack<int> onHold;
-  int passed = 0;
+int main(){
+
+  vector<vector<int>> tracking {};
+  vector<vector<int>> out{};
+  int time = 0;
+
+  /*
+  string inputs[] = {
+    "R 12",
+    "W 2",
+    "R 23",
+    "W 3",
+    "R 45",
+    "S 45",
+    "R 45",
+    "S 23",
+    "R 23",
+    "W 2",
+    "S 23",
+    "R 34",
+    "S 12",
+    "S 34"
+  };
+  */
+
+  string inputs[]{
+    "R 2",
+    "R 3",
+    "W 5",
+    "S 2",
+    "S 3"
+  };
+  int in = 5;
+
   for(int i = 0; i < in; i++){
-    string line;
-    cin >> line;
+    cout << "\n\n TIME: " << time << "\n\n";
+    string line = inputs[i];
+
+
     char mode = line[0];
-    int other = int(line[2]);
+    int other;
+    if(line.size() > 3){
+      string temp;
+      for(int k = 2; k < line.size(); k++){
+        temp[k-2] = line[k];
+      }
+      other = stoi(temp);
+    } else {
+      other = int(line[2]) - 48;
+    }
 
     if(mode == 'R'){
-      bool exsists = false;
-      int x;
 
-      //check if already exsists else;
-      
-      for(x = 0; x < tracking.size(); x++){
-        if(tracking[x][0] == other){
-          exsists = true;
+      bool exisi = false;
+      vector<int> p = {other, time};
+      tracking.push_back(p);
+
+      for(int x = 0; x < out.size(); x++){
+        if(out[x][0] == other){
+          exisi = true;
           break;
         }
       }
-      if(exsists){
-        break;
-      } else {
-        tracking.push_back(vector<int> p = {other, passed});
-        onHold.push(tracking.size() - 1);
+
+      if(!exisi){
+        out.push_back({other, 0});
       }
+
+
+      cout << "ADDED NEW ROW: \n";
+      vecout(tracking);
+      cout << "END OF NEW ROW ADDITION \n";
+      cout << "ADDED NEW ROW BUT OUT: \n";
+      vecout(out);
+      cout << "END OF NEW ROW ADDITION \n";
     }
 
     if(mode == 'W'){
-      while(!onHold.empty()){
-        tracking[onHold.top()][1] += 1 + passed;
-        onHold.pop();
-      }
-      passed = 0;
+      time += other - 1;
+      continue;
     }
 
+    if(mode == 'S'){
+      //find it in tracking
+      int received;
+      int sent = time;
+      for(int x = 0; x < tracking.size(); x++){
+        if(tracking[x][0] == other){
+          received = tracking[x][1];
+          tracking.erase(tracking.begin() + x);
+          break;
+        }
+      }
+      for(int x = 0; x < out.size(); x++){
+        if(out[x][0] == other){
+          out[x][1] += sent - received;
+          break;
+        }
+      }
+    }
 
-    passed++;
+    time++;
   }
+
+  if(tracking.size() > 0){
+    for(int x = 0; x < tracking.size(); x++){
+      int id = tracking[x][0];
+      for(int y = 0; y < out.size(); y++){
+        if(id == out[y][0]){
+          out[y][1] = -1;
+        }
+      }
+    }
+  }
+  //print out
+  cout << "\nTRACKING:\n";
+  vecout(tracking);
+  cout << "\n OUTPUT: \n";
+  vecout(out);
 }
